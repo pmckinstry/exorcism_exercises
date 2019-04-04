13eac0c052f88fb52f34a3c809bc2f708677f4ff
Pete McKinstry <pete@i90trust.com>
2019-04-03 18:00:14 -0700

    python high score exercise added

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,3 +1,28 @@
 class HighScores(object):
     def __init__(self, scores):
-        pass
+        self._scores = scores
+
+    @property
+    def scores(self):
+        return self._scores
+
+    @scores.setter
+    def scores(self, scores):
+        self._scores = scores
+
+    # return latest score by getting most recent score (last element)
+    def latest(self):
+        if len(self.scores) == 0:
+            return None
+        return self.scores[-1]
+
+    # return personal best by sorting list & then returning the last element - the highest
+    def personal_best(self):
+        if len(self.scores)==0:
+            return None
+        return sorted(self.scores)[-1]
+
+    # return personal top 3 by sorting list in reverse & then returning the first three elements.
+    # note that python slices gracefully handle out of range conditions.
+    def personal_top_three(self):
+        return sorted(self.scores, reverse=True)[0:3]

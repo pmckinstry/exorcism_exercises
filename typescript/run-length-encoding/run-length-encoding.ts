28a9fdc5591f12966d6b9b503f6b536541559549
Pete McKinstry <pete@i90trust.com>
2019-06-05 20:57:09 -0700

    fix security problem

--- typescript/run-length-encoding/run-length-encoding.ts
+++ typescript/run-length-encoding/run-length-encoding.ts
@@ -1,6 +1,52 @@
+
 class RunLengthEncoding {
-  encode(message: string) {
-    return message
+  static encode(input: string) {
+    let output = '';
+    let count = 0
+    let previous = null
+    for (let i=0; i < input.length; ++i) {
+      let current = input[i]
+      if (previous && current != previous) { // new character, append to output
+        if (count > 1) {
+          output += count
+        }
+        output += previous
+        count = 1 // reset
+      } else {
+        ++count
+      }
+      previous = current
+    }
+    if (previous) {
+      if (count > 1) {
+        output += count
+      }
+      output += previous
+    }
+    return output
+  }
+
+  static decode(input: string) {
+    let output = '';
+    let count = 0
+    for (let i=0; i < input.length; ++i) {
+      let current = input[i]
+      if (!parseInt(current)) {
+        if (count > 0) { // append this character 'count' times.
+          output += current.repeat(count)
+        } else {
+          output += current
+        }
+        count = 0 // reset
+      } else {
+        if (count > 0) {
+          count *= 10 // shift digits
+        }
+        count += parseInt(current)
+      }
+
+    }
+    return output
   }
 }
 
